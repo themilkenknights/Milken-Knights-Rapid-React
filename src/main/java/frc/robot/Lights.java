ed4922b03d46d652ff757040c367b10947612906
bossmaster217 <92772910+bossmaster217@users.noreply.github.com>
2022-02-28 16:01:36 -0800

    lights camera action wiggles

---
+++ src/main/java/frc/robot/Lights.java
@@ -0,0 +1,118 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Constants.LIGHTS;
+
+/**
+ * Lights class contains everything relating to ergabled
+*/
+public class Lights {
+    private double navXRot = 0;
+    private AddressableLED LEDS = new AddressableLED(LIGHTS.PWMPORT);
+    private AddressableLEDBuffer buffer = new AddressableLEDBuffer(LIGHTS.bufferNum);
+    private Drive mDrive = Drive.getInstance();
+    private int offset;
+    private Timer timer = new Timer();
+
+    private Lights()
+    {
+        LEDS.setLength(LIGHTS.bufferNum);
+        LEDS.setData(buffer);
+        LEDS.start();
+    }
+
+    public static Lights getInstance()
+    {
+        return InstanceHolder.mInstance;
+    }
+    
+
+    public void french() {
+        // For every pixel
+        for (var i = 0; i < LIGHTS.bufferNum; i++) 
+        {   
+                timer.start();
+                if(i < (LIGHTS.bufferNum / 3))
+                {
+                    buffer.setRGB((i+offset)%LIGHTS.bufferNum, 0, 0, 255);
+                }
+                else if(i >= (LIGHTS.bufferNum / 3) && i < ((2 * LIGHTS.bufferNum) / 3))
+                {
+                    buffer.setRGB((i+offset)%LIGHTS.bufferNum, 255, 255, 255);
+                }
+                else
+                {
+                    buffer.setRGB((i+offset)% LIGHTS.bufferNum, 255, 0, 0);
+                }
+                if(timer.get() > 0.08)
+                {
+                    offset = (offset + 1) % LIGHTS.bufferNum;
+                    timer.reset();
+                    LEDS.setData(buffer);
+                }
+            
+        }
+      }
+
+      //me version
+      public void lilNavX()
+      {
+          navXRot = (((mDrive.getNavx() + 360) % 360) * LIGHTS.bufferNum/360);
+          for(double i = 0; i < LIGHTS.bufferNum; i++)
+          {
+              buffer.setRGB(((int)i + 100) % 100, 0, 0, 0);
+              if(!(i > (((navXRot + LIGHTS.bufferNum) % LIGHTS.bufferNum) + 5) || i < (((navXRot + LIGHTS.bufferNum) % LIGHTS.bufferNum) - 5)))
+              {
+                buffer.setRGB(((int)i + 100) % 100, 255, 255, 255);
+              }
+              if(navXRot < 5 || navXRot > 95)
+              {
+                for(double j = navXRot - 5; j < navXRot + 5; j++)
+                {
+                    buffer.setRGB(((int)j + 100) % 100, 255, 255, 255);
+                }
+              } 
+                
+          }
+          LEDS.setData(buffer);
+      }
+
+      //setphan version
+      public void lilNavXTWO()
+      {
+        navXRot = (((mDrive.getNavx() + 360) % 360) * LIGHTS.bufferNum/360);
+        for(int i = 0; i < LIGHTS.bufferNum; i++)
+        {
+            if(((i + 10 / 2 - (int)navXRot) + LIGHTS.bufferNum) % LIGHTS.bufferNum < 10)
+            {
+                buffer.setRGB(i, 255/2, 255/2, 255/2);
+            }
+            else
+            {
+                buffer.setRGB(i, 0, 0, 0);
+            }
+        }
+        LEDS.setData(buffer);
+      }
+
+
+      public void voltage(double volts)
+      {
+          for(int i = 0; i < LIGHTS.bufferNum; i++)
+          {
+              buffer.setRGB(i, (int)(((((volts - 11)/13) * 255))/2), 0, 0);
+          }
+          LEDS.setData(buffer);
+      }
+
+    private static class InstanceHolder
+    {
+        private static final Lights mInstance = new Lights();
+    } 
+}
