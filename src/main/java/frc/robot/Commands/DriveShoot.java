53526020e381b8e23241bcf5ec8a53756d0907b2
MilkenRobotics <100170064+MilkenRobotics@users.noreply.github.com>
2022-03-05 14:42:32 -0800

    auto mobile

--- src/main/java/frc/robot/Commands/DriveShoot.java
+++ src/main/java/frc/robot/Commands/DriveShoot.java
@@ -12,24 +12,15 @@ import frc.robot.Commands.Commandments.Turn;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class DriveComp extends SequentialCommandGroup {
+public class DriveShoot extends SequentialCommandGroup {
+  /** Creates a new DriveShoot. */
   private Drive mDrive = Drive.getInstance();
   private double distanceA = -120;
   private double angle = 0;
-
-
-  public DriveComp() {
+  public DriveShoot() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-
-    //// still need to see if angle is either 90 - angle and 90 + angle or -angle and angle
-    //// determining if its a unit circle 0 ordeal or a top y axis = 0 ordeal
-    //sticking with wpi swervecommandcontroller for now, if that doesnt work then try and fix homemade auto
-
     addCommands(deadline(new Turn(0).withTimeout(1)), 
                 deadline(new DriveStraightREALSUPERREAL(distanceA, angle).withTimeout(6)));
-
-                //TODO see if above still works, then do this
-      //addCommands(deadline(new DriveStraightREAL(10, 1, 0)));
   }
 }
