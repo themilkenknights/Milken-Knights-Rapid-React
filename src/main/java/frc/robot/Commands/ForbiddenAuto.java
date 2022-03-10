0c82278e8c88e02b5aa21b9ecd047a30eb618a92
bossmaster217 <92772910+bossmaster217@users.noreply.github.com>
2022-03-10 08:40:52 -0800

    use after adding xbox angle converter in the auto

---
+++ src/main/java/frc/robot/Commands/ForbiddenAuto.java
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Commands.Commandments.DriveStraightREAL;
+import frc.robot.Commands.Commandments.DriveStraightREALSUPERREAL;
+import frc.robot.Constants.DRIVE;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class ForbiddenAuto extends SequentialCommandGroup {
+  /** Creates a new ForbiddenAuto. */
+  private double inches  = 21;
+  private double FWD = 1;
+  private double STR = 0;
+  private double RCW = 0.2;
+  private double maxVelo = DRIVE.magicVelo;
+  private double maxAccel = DRIVE.magicAccel;
+  public ForbiddenAuto() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(deadline(new DriveStraightREAL(inches, FWD, STR, RCW, maxVelo, maxAccel).withTimeout(6)));
+  }
+}
