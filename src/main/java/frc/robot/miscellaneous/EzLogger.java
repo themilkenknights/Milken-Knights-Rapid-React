0d246036d7ff0736647434bb757a6a005c6867c3
bossmaster217 <92772910+bossmaster217@users.noreply.github.com>
2022-03-16 22:04:33 -0700

    gromit mug

--- src/main/java/frc/robot/miscellaneous/EzLogger.java
+++ src/main/java/frc/robot/miscellaneous/EzLogger.java
@@ -27,6 +27,9 @@ public class EzLogger {
     private static File todaysFolder = new File(staticGetYourFilePath() + "\\logs\\" + staticGetTodaysDate());
  /**le bully ascii art*/
     public static String bully = ("                                    .7Y?^./n                                   .7P5?^.:.                     .::::          7Y7!75#B5YY~/n                   ^:^7?77~.         .::7@@@@@@&?./n                  !?^~!??Y7.    :?YYJ?!~:!Y#@@&&@BY~./n                .J#G5Y5!:.     !5Y??PB##BGJ^7B@@&&&&G?./n              .YB#&@@&!       :5?7775#&&&&&B! 7B@&&&#&G~/n              J@@&@@@!        :?YPY?75&&&B#@G   !B@&&&&B^/n             7@@@@@@?         ~J?!7?JJJJJJ#@5    :&&&&@@G./n            !@@@@@@?          .YY?JYY5Y5P&&J.    Y@@@@@@5./n           ~&@&@@@B.           !Y5PPBBPJJBJ     !@@@@@@@J/n          :B&&&@@@P:      .^^:.:!7Y&&##BB##? ^YG&@@@@@@B^/n          !@@&@@@&&#G5?!~?B&&###BPG@@&@@@&@&Y#@@@@@@@@&7/n          :Y&@@@@@@@@@@@&@#&&&&&&&@&&#&&&@&&##&@@@@@@@J/n            :?B@@@@@@@@@@@###&&##&&#BB#&&&##B#@@@@@@&Y./n               ^7YG#@@@@@&####&&&&####&&&&##&&&&&@@@Y/n                   .^?5GBB#B###&@&&@@@&&&&&&&&&&&&&J./n                        .J##BB&@@@&&&#&&&@@&&&&&&&G./n                        ^B###&@@&&###&&&@@&&&@&&&&G./n                        ?##&@@@&###&&&&&&&&&&@&&&&&J/n                       ^G##@@@&&##&&#&&#&&@@@@&&&&&&!/n                       J##@@@&##B#&####&&&&@&@&&&&&&G./n                      7B#@@@&#########&&&@@&&&&&&&&&&G!/n                     ~B&@@@&&#&&&&&&&&&&@@@&&&&&&&&&&&Y./n                    :P#@@@@&&&&&&&&&&&&@&##@&&&&&&&&&&G^/n                   :P&@@&&###&&&&&&&&&@&!.^&&&&&&&&&&&@5./n                  ~5&@@&######&&&&&&&&&#?: Y&&&&&&&&&&@#!/n                 ^Y#@&&&&####&&&&&&&&&&&@&Y~B&&&&&&&&&&&#7/n                 !G##&&&#####&&&&@@&&&&&&##5!B&&&&&&&&&&@B./n                :5###&&&&&########&&&&&&@7:. ^G&@@@@&&&#G!/n               ^P######&&&&&############&#5:  .!Y55J7~:./n              ^G##&&&&&&&&&B&&############&G:/n             ^G##&&&&&@@@#!:Y&&&&#########&@5/n             Y&##&#&&&&@@!   7B&#&&&&#####&&B:/n            ~###&&#&&&&@Y.    :P&&###&&&&&&&&7/n            J#####&&&&@5       ^B&&###&&&&&&@5/n           :B###&&&&&@#~       .~G&&&&#&&&&&&?/n           ?####&&&@@#~.        ^!P&&&&&&&&&&Y/n");
+    
+    public static String gromit = ("                  NKkxddoddxO0OxxkkkOOO000K00000OOOOOOOOOkxxx0N>               W0o;'''',,,,,;codxxkkkkOOOkOOOOOOkkkkkkOkkkxdoolxX>             W0c............':oddxxkkkkkOOOkkkkkkOOOkkkkkkxdol,.oN>            Nd'......,ldxkkdollodxxkkkO000OOOkkO0OkO0Okkkxxdol,.;K>           No......,dKW     WOoodxxkkkOkl,.'oOOkc'.'dOkkkxxdol,.;K>          Wx......;OW        KdoxxkkkOOkc'.,oOOkc'.,okkkkxxdoc,.:K>          0;.....'xW         XxdxxkkkOOkkxxkOO00OxxxkkkOkkxdoc'.cX>        WWx......cK          XxdxkkkkOOOOOOOOOOOOOOOOOOkkkxdo:'.cX>        NXl......xN          KxxxxkkkkkOOOOOOOOOOOOOOOkkkkxdo:'.:K>        NKl......oX         W0xxxkkkkkkOOOOO000000OOOOOkkkxxol,.'kW>        WXo......,kN       WKkxxxkkkkOOOOOOO0000000OOOOOOkkxxoc'.cX>         Wk' .....;kN      XkdxxkkkkOOOO000000OOOOOOOOOOOOOkxxo:..kW>          Xc.......'l0N   NkddxxkkkkOOO000OxxdddxkkOOOOOOOOkkxdl,.cN>           0:........,lk0KkodxxkkkkkOOkdc,.......';cdkOOOOOOkkxo:.:K>           WKc..........';codxxkkkkkkx:.    ....   ..;dOOOOOkkxoc.cX>             Xo,..........:odxxxkkkkx:.    ....      .'okOkkkkxol:xW>              NOc.........;odxxkkkkko'      .         .;xkkkkxxxdkX>               WXkl,......,oxxxxkkkko'                .,dkkkkxxkKN>                  WXOxolloxkxdxxxxxkd:.     ..       ..cxkxxxxkKW>                          W0xdddxxxxd;..           ..:dxxxxxkKW>                           W0xodddddddl;'...   ...';lddddddkKW>                            WXkddddddddooc:;;;;;:cloooooodkXW>                              WNXKKK000OOOOkxddxkkOOOOOO0XW>                                MMMMMMMMMMWWWMMMMMMMMMMM>                                                             >");
+    
     public static File todaysLog = new File(ultimatePath + "\\logs\\" + todaysDate + "\\main_log.txt");
 
 
@@ -165,7 +168,7 @@ public static void main(String[] args) {
     staticLogRobotInit();
 
         //bully creator lol
-        for(int i = 0; i < 37; i++)
+        /*for(int i = 0; i < 37; i++)
         {
             //System.out.println(bully.indexOf("/n"));
             String temp = bully;
@@ -173,6 +176,16 @@ public static void main(String[] args) {
             staticWriteLog(bully);
             bully = temp;
             bully = bully.substring(bully.indexOf("/n")+2, bully.length());
+        }
+*/
+        for(int i = 0; i < 25; i++)
+        {
+            //System.out.println(bully.indexOf("/n"));
+            String temp = gromit;
+            gromit = gromit.substring(0, gromit.indexOf(">"));
+            staticWriteLog(gromit);
+            gromit = temp;
+            gromit = gromit.substring(gromit.indexOf(">")+1, gromit.length());
         }
 
         cheatTheSystem(cheat());
