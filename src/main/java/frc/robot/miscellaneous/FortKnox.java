100a9e297d301d3327bd83dc6fd0db3d94ecd49a
bossmaster217 <92772910+bossmaster217@users.noreply.github.com>
2022-03-18 12:04:27 -0700

    Create FortKnox.java

---
+++ src/main/java/frc/robot/miscellaneous/FortKnox.java
@@ -0,0 +1,10 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.miscellaneous;
+
+/** Add your docs here. */
+public class FortKnox {
+    //by the hour, next the universal number, next to desire plural without zero, next to another universal number minus a sun dog mimicing our first creation, next to the sign of evil or good universal, over the graceful creation, inside a modded minecraft server
+}
