4191c0fa7131dbe86b383578a06007adf34fac30
bossmaster217 <92772910+bossmaster217@users.noreply.github.com>
2022-03-24 23:28:41 -0700

    sim diy
    wont work lol

--- src/main/java/frc/robot/miscellaneous/Simulation.java
+++ src/main/java/frc/robot/miscellaneous/Simulation.java
@@ -4,5 +4,36 @@
 
 package frc.robot.miscellaneous;
 
+import frc.robot.Drive;
+import frc.robot.Constants.DRIVE;
+
 /** Add your docs here. */
-public class Simulation {}
+public class Simulation {
+    private Odometry mOdo = Odometry.getInstance();
+    private Drive mDrive = Drive.getInstance();
+    private double distanceA = 30;
+    private double lengthB = 30;
+    private double distance = mDrive.calculateArcOfPath(distanceA, lengthB);
+    private double angle = mDrive.calculateAngleOfPath(distanceA, lengthB);
+    private double loopTime = 0.02;
+    //https://www.omnicalculator.com/physics/acceleration
+
+    public static Odometry getInstance()
+    {
+        return InstanceHolder.mInstance;
+    }
+
+    public void updateVelocity()
+    {
+        //or do tan of distances x and y to find actual distance of motor
+        double tesDist = Math.sqrt(Math.pow(mOdo.getX(), 2) + Math.pow(mOdo.getY(), 2));
+        double testRot = (tesDist / distance) * angle;
+        double rot = (((mOdo.getX() / distanceA) + (mOdo.getY() / lengthB)) / 2) * angle;
+        mOdo.drive(DRIVE.maxNativeVelocity * Math.cos(angle), DRIVE.maxNativeVelocity * Math.cos(angle), rot, true);
+    }
+
+    private static class InstanceHolder
+    {
+        private static final Odometry mInstance = new Odometry();
+    } 
+}
