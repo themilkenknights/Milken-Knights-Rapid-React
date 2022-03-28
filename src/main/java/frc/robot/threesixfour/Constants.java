bd11e5fcbf1205633dcb2e5b64c5283d86304677
Jack Greer <jackxgreer@icloud.com>
2022-03-28 14:56:18 -0700

    364 canid

--- src/main/java/frc/robot/threesixfour/Constants.java
+++ src/main/java/frc/robot/threesixfour/Constants.java
@@ -79,9 +79,9 @@ public final class Constants {
         /* Module Specific Constants */
         /* Front Left Module - Module 0 */
         public static final class Mod0 {
-            public static final int driveMotorID = 1;
-            public static final int angleMotorID = 2;
-            public static final int canCoderID = 1;
+            public static final int driveMotorID = 3;
+            public static final int angleMotorID = 16;
+            public static final int canCoderID = 4;
             public static final double angleOffset = 37.35;
             public static final SwerveModuleConstants constants = 
                 new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
@@ -89,9 +89,9 @@ public final class Constants {
 
         /* Front Right Module - Module 1 */
         public static final class Mod1 {
-            public static final int driveMotorID = 3;
-            public static final int angleMotorID = 4;
-            public static final int canCoderID = 2;
+            public static final int driveMotorID = 5;
+            public static final int angleMotorID = 18;
+            public static final int canCoderID = 6;
             public static final double angleOffset = 10.45;
             public static final SwerveModuleConstants constants = 
                 new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
@@ -99,9 +99,9 @@ public final class Constants {
         
         /* Back Left Module - Module 2 */
         public static final class Mod2 {
-            public static final int driveMotorID = 5;
-            public static final int angleMotorID = 6;
-            public static final int canCoderID = 3;
+            public static final int driveMotorID = 2;
+            public static final int angleMotorID = 15;
+            public static final int canCoderID = 1;
             public static final double angleOffset = 38.75;
             public static final SwerveModuleConstants constants = 
                 new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
@@ -110,8 +110,8 @@ public final class Constants {
         /* Back Right Module - Module 3 */
         public static final class Mod3 {
             public static final int driveMotorID = 7;
-            public static final int angleMotorID = 8;
-            public static final int canCoderID = 4;
+            public static final int angleMotorID = 17;
+            public static final int canCoderID = 8;
             public static final double angleOffset = 58.88;
             public static final SwerveModuleConstants constants = 
                 new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
